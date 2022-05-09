1b2a931354fde5d59272340dc88a4fb94d3c1560
jdamaro <76124774+jdamaro@users.noreply.github.com>
2022-05-09 19:00:42 -0400

    Initialize Repo

---
+++ JVGuttag/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Intro to Computation and Programming Using Python',
+    author='John D'Amaro',
+    license='MIT',
+)
